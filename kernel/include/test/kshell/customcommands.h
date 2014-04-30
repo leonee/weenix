99c271390bba43156c2df52fc7a021e95e351b2d
arkleima <foo@server.fake>
2014-04-30 01:10:43 -0400

    added exec-related files

---
+++ kernel/include/test/kshell/customcommands.h
@@ -0,0 +1,2 @@
+#pragma once
+int kshell_exec(kshell_t *ksh, int argc, char **argv);
---
+++ kernel/test/kshell/customcommands.c
@@ -0,0 +1,51 @@
+#include "errno.h"
+#include "util/debug.h"
+
+#include "api/exec.h"
+
+#include "fs/open.h"
+#include "fs/fcntl.h"
+
+#include "test/kshell/io.h"
+
+#include "proc/proc.h"
+#include "proc/kthread.h"
+
+#ifdef __VM__
+static void * exec_func(int arg1, void *arg2){
+    char *func_to_run = (char *) arg2;
+
+    /* open stdin, stdout, stderr */
+    KASSERT(do_open("/dev/tty0", O_RDONLY) == 0);
+    KASSERT(do_open("/dev/tty0", O_WRONLY) == 1);
+    KASSERT(do_open("/dev/tty0", O_WRONLY) == 2);
+ 
+    char *empty_args[1] = {NULL};
+    char *empty_envp[1] = {NULL};
+    kernel_execve(func_to_run, empty_args, empty_envp);
+
+    panic("returned when you weren't supposed to!");
+
+    return NULL;
+}
+
+int kshell_exec(kshell_t *ksh, int argc, char **argv){
+    KASSERT(NULL != ksh);
+    KASSERT(NULL != argv);
+
+    if (argc != 2){
+        kprintf(ksh, "Usage: exec <command>\n");
+        return 1;
+    }
+
+    proc_t *execproc = proc_create("exec_proc");
+    kthread_t *execthread = kthread_create(execproc, exec_func, 0, argv[1]);
+
+    sched_make_runnable(execthread);
+
+    int status;
+    do_waitpid(execproc->p_pid, 0, &status);
+
+    return 0;
+}
+#endif
