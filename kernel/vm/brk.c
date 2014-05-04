35eabee6076441850105b3efc2d174d956813768
arkleima <foo@server.fake>
2014-05-03 21:13:10 -0400

    second shot at do_brkc

--- kernel/vm/brk.c
+++ kernel/vm/brk.c
@@ -63,15 +63,32 @@ do_brk(void *addr, void **ret)
         return 0;
     }
 
-    uint32_t old_brk = (uint32_t) curproc->p_start_brk;
-
     if (addr < curproc->p_start_brk || (uint32_t) addr > USER_MEM_HIGH){
         return -ENOMEM;
     }
 
+    uint32_t old_brk = (uint32_t) curproc->p_brk;
+
     int brk_end_page;
 
-    if (addr > curproc->p_brk){
+    if (addr <= curproc->p_brk){
+        /* if it's not page-aligned, then addr does not lie on a page
+         * boundary, so we actually need to have our mapping include
+         * the page that addr is in. This is exclusive, so it's the first
+         * page to unmap */
+        brk_end_page = ADDR_TO_PN(addr) + !PAGE_ALIGNED(addr);
+        
+        uint32_t old_brk_end_page = ADDR_TO_PN(old_brk) + !PAGE_ALIGNED(old_brk);
+
+        uint32_t npages = old_brk_end_page - brk_end_page;
+
+        vmmap_remove(curproc->p_vmmap, brk_end_page, npages);
+
+        curproc->p_brk = addr;
+        *ret = addr;
+        return 0;
+    } else {
+        KASSERT(addr > curproc->p_brk);
 
         /* the first page of the new brk area that isn't part of the vma
          * for the brk area already. If brk isn't page aligned, then the page
@@ -81,7 +98,7 @@ do_brk(void *addr, void **ret)
         uint32_t first_new_page = ADDR_TO_PN(old_brk) + !PAGE_ALIGNED(old_brk);
 
         /* exclusive */
-        brk_end_page = ADDR_TO_PN(addr) + !PAGE_ALIGNED(addr);
+        brk_end_page = ADDR_TO_PN(addr) - PAGE_ALIGNED(addr);
 
         uint32_t npages = brk_end_page - first_new_page;
 
@@ -92,19 +109,20 @@ do_brk(void *addr, void **ret)
         /* try to catch off-by-one errors by making sure the last page in 
          * the new brk area doesn't have a mapping */
         KASSERT(vmmap_is_range_empty(curproc->p_vmmap, brk_end_page - 1, 1));
-    } else {
-        /* if it's not page-aligned, then addr does not lie on a page
-         * boundary, so we actually need to have our mapping include
-         * the page that addr is in */
-        brk_end_page = ADDR_TO_PN(addr) + !PAGE_ALIGNED(addr);
-    }
-    
-    vmarea_t *vma =
+
+        vmarea_t *vma =
             vmmap_lookup(curproc->p_vmmap, ADDR_TO_PN(curproc->p_start_brk));
 
-    KASSERT(vma != NULL);
+        if (vma == NULL){
+            vmmap_map(curproc->p_vmmap, NULL, ADDR_TO_PN(curproc->p_brk),
+                    brk_end_page - ADDR_TO_PN(curproc->p_start_brk),
+                    PROT_READ | PROT_WRITE, 
+                    MAP_PRIVATE, 0, VMMAP_DIR_LOHI, &vma);
+        } else {
+            vma->vma_end = brk_end_page;
+        }
+    }
 
-    vma->vma_end = brk_end_page;
     curproc->p_brk = addr;
     *ret = addr;
     return 0;
